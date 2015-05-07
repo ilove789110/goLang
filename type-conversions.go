386e6bfbc6501c2b8e2577a23c2d9a4c00cd1f59
ilove789110 <ilove789110@163.com>
2015-05-07 18:54:55 +0800

    commit

--- type-conversions.go
+++ type-conversions.go
@@ -10,4 +10,10 @@ func main() {
 	var f float64 = math.Sqrt(float64(x*x + y*y))
 	var z int = int(f)
 	fmt.Println(x, y, z)
+
+	var i int = 42
+	var j float64 = float64(i)
+
+	fmt.Println(float64(i) + j) //任何类型的隐式类型转换都会造成编译报错
+
 }
