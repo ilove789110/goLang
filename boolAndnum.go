bc12ca27b6c78d039c0917ed89ca016b0235e8f5
ilove789110 <ilove789110@163.com>
2015-06-25 16:20:00 +0800

    add boolAndnum.go

---
+++ boolAndnum.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	Cyan    = 1
+	Magenta = 2
+	Yellow  = 0
+	Black   = iota /*iota的特性：遇到const关键字重置为0；在const的第一行出现，iota=0；在第二行出现，iota=1；依此类推*/
+	White
+	Red
+)
+
+type BitFlag int
+
+const (
+	Active  BitFlag = 1 << iota
+	Send            //隐式设置成BitFlag=1<<iota 1<<1=2
+	Receive         //隐式设置成BitFlag=1<<iota 1<<2=2
+)
+
+func main() {
+	fmt.Println(Cyan)
+	fmt.Println(Magenta)
+	fmt.Println(Yellow)
+	fmt.Println(Black)
+	fmt.Println(White)
+	fmt.Println(Red)
+
+	fmt.Println(Active)
+	fmt.Println(Send)
+	fmt.Println(Receive)
+}
--- my_bigdigits.go
+++ my_bigdigits.go
@@ -1,49 +1,49 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-)
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"path/filepath"
+// )
 
-var bigDigits = [][]string{
-	{"  000  ", " 0   0 ", "0     0", "0     0", "0     0", " 0   0 ", "  000  "},
-	{" 1 ", "11 ", " 1 ", " 1 ", " 1 ", " 1 ", "111"},
-	{" 333 ", "3   3", "    3", "   33", "    3", "3   3", " 333 "},
-}
+// var bigDigits = [][]string{
+// 	{"  000  ", " 0   0 ", "0     0", "0     0", "0     0", " 0   0 ", "  000  "},
+// 	{" 1 ", "11 ", " 1 ", " 1 ", " 1 ", " 1 ", "111"},
+// 	{" 333 ", "3   3", "    3", "   33", "    3", "3   3", " 333 "},
+// }
 
-func main() {
+// func main() {
 
-	if len(os.Args) == 1 || (len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help")) {
-		fmt.Printf("usage: %s [-b|--bar] <whole-number>\n", filepath.Base(os.Args[0]))
-		fmt.Print("-b -bar draw an underbar and an overbar")
-		os.Exit(1)
-	}
+// 	if len(os.Args) == 1 || (len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help")) {
+// 		fmt.Printf("usage: %s [-b|--bar] <whole-number>\n", filepath.Base(os.Args[0]))
+// 		fmt.Print("-b -bar draw an underbar and an overbar")
+// 		os.Exit(1)
+// 	}
 
-	lenOSArgs := len(os.Args)
+// 	lenOSArgs := len(os.Args)
 
-	if lenOSArgs == 3 && (os.Args[1] == "-b" || os.Args[1] == "--bar") { /*draw an overbar*/
-		fmt.Println("*************************************************************************")
-	}
+// 	if lenOSArgs == 3 && (os.Args[1] == "-b" || os.Args[1] == "--bar") { /*draw an overbar*/
+// 		fmt.Println("*************************************************************************")
+// 	}
 
-	stringOfDigits := os.Args[lenOSArgs-1]
-	for row := range bigDigits[0] {
-		line := ""
-		for column := range stringOfDigits {
-			digit := stringOfDigits[column] - '0'
-			/*Go中字符串编码为UTF-8，字符'0'对应的是48，字符'1'对应的是49。
-			一个字符是与Go的整型类型兼容的整型数，而且Go语言的数值类型会适应上下文，跟Go语言强类型且不支持隐式类型不冲突*/
-			if 0 <= digit && digit <= 9 { /*此处0和9被认定为byte类型*/
-				line += bigDigits[digit][row] + " " //Go中string类型是不可变的，但是+=和+是支持的，主要是易于使用。实质上是暗地里将字符串替换了
-			} else {
-				log.Fatal("invalid whole number")
-			}
-		}
-		fmt.Println(line)
-	}
+// 	stringOfDigits := os.Args[lenOSArgs-1]
+// 	for row := range bigDigits[0] {
+// 		line := ""
+// 		for column := range stringOfDigits {
+// 			digit := stringOfDigits[column] - '0'
+// 			/*Go中字符串编码为UTF-8，字符'0'对应的是48，字符'1'对应的是49。
+// 			一个字符是与Go的整型类型兼容的整型数，而且Go语言的数值类型会适应上下文，跟Go语言强类型且不支持隐式类型不冲突*/
+// 			if 0 <= digit && digit <= 9 { /*此处0和9被认定为byte类型*/
+// 				line += bigDigits[digit][row] + " " //Go中string类型是不可变的，但是+=和+是支持的，主要是易于使用。实质上是暗地里将字符串替换了
+// 			} else {
+// 				log.Fatal("invalid whole number")
+// 			}
+// 		}
+// 		fmt.Println(line)
+// 	}
 
-	if lenOSArgs == 3 && (os.Args[1] == "-b" || os.Args[1] == "--bar") { /*draw an underbar*/
-		fmt.Println("*************************************************************************")
-	}
-}
+// 	if lenOSArgs == 3 && (os.Args[1] == "-b" || os.Args[1] == "--bar") { /*draw an underbar*/
+// 		fmt.Println("*************************************************************************")
+// 	}
+// }
